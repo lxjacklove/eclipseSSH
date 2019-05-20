bd6bd6d0b51b84807acc940ea20708845c4a699a
lxjacklove <50793470+lxjacklove@users.noreply.github.com>
2019-05-20 15:10:24 +0800

    添加了switch case语句
    switch（）{
            case:
    }

---
+++ SRC/COM/QW/sef.java
@@ -0,0 +1,15 @@
+package com.qw;
+
+public class sef {
+	public static void main(String[] args) {
+		
+	}
+	public String getUser(String op){
+		switch(op){
+			case 1:System.out.print("谁最帅");break;
+			case 2:System.out.print("你最帅");break;
+			case 3:System.out.print("黎小江最帅");break;
+		}
+		
+	}
+}
--- src/com/qw/sef.java
+++
@@ -1,7 +0,0 @@
-package com.qw;
-
-public class sef {
-	public static void main(String[] args) {
-		System.out.println("sgrserg");
-	}
-}
